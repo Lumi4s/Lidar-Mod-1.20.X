cc03c5475625def6e2f1c44f29876f74926a2239
Lumi <lumi4p@list.ru>
2026-06-03 18:03:32 +0500

    YARN version, set this as start point

--- src/main/java/net/lumi/lidarmod/LidarModClient.java
+++ src/main/java/net/lumi/lidarmod/LidarModClient.java
@@ -3,7 +3,6 @@ package net.lumi.lidarmod;
 import net.fabricmc.api.ClientModInitializer;
 
 public class LidarModClient implements ClientModInitializer {
-
     @Override
     public void onInitializeClient() {
 
