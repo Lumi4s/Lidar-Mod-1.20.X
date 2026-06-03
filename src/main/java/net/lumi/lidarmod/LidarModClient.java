f8116f0eb4d5bb9b94bb38feb21d05465bfbdd8e
Lumi <lumi4p@list.ru>
2026-06-03 16:32:28 +0500

    Initial commit

---
+++ src/main/java/net/lumi/lidarmod/LidarModClient.java
@@ -0,0 +1,11 @@
+package net.lumi.lidarmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LidarModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/lumi/lidarmod/LidarModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.lumi.lidarmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LidarModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
